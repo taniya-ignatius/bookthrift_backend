779ff67a5fdf27e57e5464aebf869350e5909f6c
taniya-ignatius <itaniya895@gmail.com>
2024-03-17 13:23:43 +0530

    Books added successfully

--- controllers/AdminRouter.js
+++ controllers/AdminRouter.js
@@ -11,7 +11,14 @@ router.get("/viewregisteredusers",async(req,res)=>{
 
 })
 
-
+router.post("/addbooks",async(req,res)=>{
+    let data=req.body
+    let newbooks=new adminModel(data)
+    let result=await newbooks.save()
+    res.json({
+        status:"success"
+    })
+})
 
 
 module.exports=router
